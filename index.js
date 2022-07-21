1b6b20dbdb64a873f99ab2772a5c3fcbcc573094
theresatvan <theresatvan@gmail.com>
2022-07-20 20:23:21 -0700

    Set up index.html

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`));
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
