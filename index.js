ed9744c79183178f447fb64c8ad9569ce28dc869
theresatvan <theresatvan@gmail.com>
2022-07-26 19:54:51 -0700

    Initialize sockets

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+
+// Initializing socket
+const io = socketio(server);
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = 3000 || process.env.PORT;
 
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`));
 
-// Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
